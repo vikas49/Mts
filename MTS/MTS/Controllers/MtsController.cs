9132c956752fda16e5dc9530889179c2c2ffd6dd
Vikas Gupta <Vikas Gupta@DESKTOP-A9ET8L7>
2018-12-27 23:26:05 +0530

    Upload

---
+++ MTS/MTS/Controllers/MtsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MTS.Controllers
+{
+    public class MtsController : Controller
+    {
+        // GET: Mts
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ContactUS()
+        {
+            return View();
+        }
+
+        public ActionResult AboutUS()
+        {
+            return View();
+        }
+
+        public ActionResult ToolAndTackles()
+        {
+            return View();
+        }
+
+        public ActionResult ManPower()
+        {
+            return View();
+        }
+
+        public ActionResult Clients()
+        {
+            return View();
+        }
+
+        public ActionResult OurService()
+        {
+            return View();
+        }
+    }
+}
---
+++ MTS/MTS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MTS.MvcApplication" Language="C#" %>
